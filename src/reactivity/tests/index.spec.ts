74654788d823b360e0bf15f2af257263ff63c96a
邹小一 <zouxiaoyi@192.168.2.8>
2023-10-28 23:55:41 +0800

    feat: 实现effect & reactive 依赖收集&触发依赖

--- src/reactivity/tests/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { add } from "../index";
-
-describe('测试', () => {
-    it('init', () => {
-        // expect(1).toBe(1)
-        expect(add(1, 1)).toBe(2)
-    })
-})
