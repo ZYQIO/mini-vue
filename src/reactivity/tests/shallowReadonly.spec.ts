cee16dce9527e4161e769f3b30b22319ad8c6c11
邹小一 <zouxiaoyi@192.168.2.5>
2023-10-29 23:49:47 +0800

    feat: 实现 shallReadonly 功能

---
+++ src/reactivity/tests/shallowReadonly.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect, vi } from "vitest";
+import { isReadonly, shallowReadonly } from "../reactive"
+
+describe('shallowReadonly', () => {
+    it('shoule not make non-reactive properties reactive', () => {
+        const props = shallowReadonly({ n: { foo: 1 } })
+        expect(isReadonly(props)).toBe(true)
+        expect(isReadonly(props.n)).toBe(false)
+    })
+
+    it('shoule call console.warn when set', () => {
+        console.warn = vi.fn()
+        const user = shallowReadonly({
+            age: 10
+        })
+        user.age = 11
+
+        expect(console.warn).toHaveBeenCalled()
+    })
+})
