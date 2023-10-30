cc51d51e2c4000128e3a6f6cb8be1c4b366cb784
邹小一 <zouxiaoyi@192.168.2.7>
2023-10-30 22:31:56 +0800

    feat: 实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -9,7 +9,17 @@ export const App = {
 
     render() {
 
-        return h("div", "hi, " + this.msg)
+        return h("div",
+            {
+                id: 'root',
+                class: ['red', 'hard']
+            },
+            // "hi, " + this.msg
+            // string
+            // "hi, mini-vue" 
+            // Array
+            [h('p', { class: 'red' }, 'hi'), h('p', { class: 'blue' }, 'mini-vue')]
+        )
 
     },
 
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared"
 import { createComponentInstance, setupComponent } from "./component"
 
 
@@ -7,15 +8,55 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
+    debugger
 
     // 会被递归调用，在这里会判断虚拟节点的类型，看看到底是component类型，还是一个element类型
 
     // 判断是不是 element，是就去处理element类型
-    // processElement()
+    if (typeof vnode.type === 'string') {
 
-    // 去处理组件
-    processComponent(vnode, container)
+        processElement(vnode, container)
 
+    } else if (isObject(vnode.type)) {
+        // 去处理组件
+        processComponent(vnode, container)
+    }
+
+}
+
+function processElement(vnode, container) {
+    mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type)
+
+    // string array
+    const { children, props } = vnode
+
+    if (typeof children === 'string') {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        // 这个数组中每一个都是虚拟节点，对于每一个虚拟节点则
+        // 需要递归调用 patch ，判断到底是一个element 还是 componen 类型
+        mountChildren(vnode, el)
+    }
+
+    // props
+    for (const key in props) {
+        const val = props[key]
+        el.setAttribute(key, val)
+    }
+
+    container.append(el)
+
+    // document.body.appendChild(el)
+}
+
+function mountChildren(vnode, container) {
+    vnode.children.forEach(v => {
+        patch(v, container)
+    })
 }
 
 function processComponent(vnode, container) {
