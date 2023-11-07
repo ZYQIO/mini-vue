1825b7d06391cfccabda40fb6ff2828506c9ca92
邹小一 <zouxiaoyi@zouxiaoyideMacBook-Pro.local>
2023-11-07 13:39:00 +0800

    feat: 更新 element 的 children - 双端对比diff算法（3）

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -224,11 +224,14 @@ export function createRenderer(options) {
             const toBePatched = e2 - s2 + 1;
             let patched = 0;
             const keyToNewIndexMap = new Map();
+            const newIndexToOldIndexMap = new Array(toBePatched);
+            let moved = false;
+            let maxNewIndexSoFar = 0;
+            for (let i = 0; i < toBePatched; i++) newIndexToOldIndexMap[i] = 0;
 
             for (let i = s2; i <= e2; i++) {
                 const nextChild = c2[i]
                 keyToNewIndexMap.set(nextChild.key, i)
-
             }
 
             for (let i = s1; i <= e1; i++) {
@@ -258,8 +261,40 @@ export function createRenderer(options) {
                     hostRemove(prevChild.el)
                 } else {
                     // 当前节点存在，就对比
+
+                    if (newIndex >= maxNewIndexSoFar) {
+                        maxNewIndexSoFar = newIndex;
+                    } else {
+                        moved = true;
+                    }
+
+                    newIndexToOldIndexMap[newIndex - s2] = i + 1;
                     patch(prevChild, c2[newIndex], container, parentComponent, null)
                     patched++;
+                }
+            }
+
+            const increasingNewIndexSequence = moved ? getSequence(newIndexToOldIndexMap) : [];
+            let j = increasingNewIndexSequence.length - 1;
+
+            for (let i = toBePatched - 1; i >= 0; i--) {
+
+                const nextIndex = i + s2;
+                const nextChild = c2[nextIndex];
+                const anchor = nextIndex + 1 < l2 ? c2[nextIndex + 1].el : null;
+
+                // 创建
+                if(newIndexToOldIndexMap[i] === 0) {
+
+                    patch(null, nextChild, container, parentComponent, anchor)
+
+                } else if (moved) {
+                    if (j < 0 || i !== increasingNewIndexSequence[j]) {
+                        console.log('移动位置');
+                        hostInsert(nextChild.el, container, anchor)
+                    } else {
+                        j--;
+                    }
                 }
             }
         }
@@ -375,3 +410,43 @@ export function createRenderer(options) {
 }
 
 
+function getSequence(arr: number[]): number[] {
+    const p = arr.slice();
+    const result = [0];
+    let i, j, u, v, c;
+    const len = arr.length;
+    for (i = 0; i < len; i++) {
+        const arrI = arr[i];
+        if (arrI !== 0) {
+            j = result[result.length - 1];
+            if (arr[j] < arrI) {
+                p[i] = j;
+                result.push(i);
+                continue;
+            }
+            u = 0;
+            v = result.length - 1;
+            while (u < v) {
+                c = (u + v) >> 1;
+                if (arr[result[c]] < arrI) {
+                    u = c + 1;
+                } else {
+                    v = c;
+                }
+            }
+            if (arrI < arr[result[u]]) {
+                if (u > 0) {
+                    p[i] = result[u - 1];
+                }
+                result[u] = i;
+            }
+        }
+    }
+    u = result.length;
+    v = result[u - 1];
+    while (u-- > 0) {
+        result[u] = v;
+        v = p[v];
+    }
+    return result;
+}
