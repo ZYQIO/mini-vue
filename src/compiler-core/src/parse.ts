0cb9c119008d537df2444e64e90b65c8a43006db
邹小一 <zouxiaoyi@zouxiaoyideMacBook-Pro.local>
2023-11-07 23:30:11 +0800

    feat: 实现解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,9 @@
 import { NodeTypes } from "./ast"
 
+const enum TagType {
+    Start,
+    End
+}
 
 export function baseParse(content: string) {
 
@@ -13,13 +17,43 @@ function parseChildren(context) {
     const nodes: any[] = []
 
     let node;
-    if (context.source.startsWith('{{')) {
+    const s = context.source;
+    if (s.startsWith('{{')) {
         node = parseInterpolation(context)
+    } else if (s[0] === "<") {
+        if (/[a-z]/i.test(s[1])) {
+            node = parseElement(context);
+        }
     }
 
     nodes.push(node)
 
     return nodes
+}
+
+function parseElement(context) {
+    const element = parseTag(context, TagType.Start)
+
+    parseTag(context, TagType.End)
+
+    return element;
+
+}
+
+function parseTag(context, type: TagType) {
+    // 1. 解析 tag
+    const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+    const tag = match[1]
+    // 2. 删除处理完成的代码
+    advanceBy(context, match[0].length)
+    advanceBy(context, 1)
+
+    if (type === TagType.End) return;
+
+    return {
+        type: NodeTypes.ELEMENT,
+        tag
+    }
 }
 
 function parseInterpolation(context) {
@@ -39,8 +73,6 @@ function parseInterpolation(context) {
     const content = rawContent.trim();
 
     advanceBy(context, rawContentLength + closeDelimiter.length)
-
-    console.log('context.source', context.source);
 
     return {
         type: NodeTypes.INTERPOLATION,
